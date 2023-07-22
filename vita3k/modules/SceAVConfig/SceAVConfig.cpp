e14174ccd474e62d09edd7920cb8b6c0d6d2ccd3
CoffeeBrewer64 <mrdirtbloc@gmail.com>
2023-07-22 22:06:43 +0100

    Tracy: Add Tracy for SceAVConfig

--- vita3k/modules/SceAVConfig/SceAVConfig.cpp
+++ vita3k/modules/SceAVConfig/SceAVConfig.cpp
@@ -17,139 +17,177 @@
 
 #include "SceAVConfig.h"
 
+#include <util/tracy.h>
+
+TRACY_MODULE_NAME(SceAVConfig);
+
 EXPORT(int, sceAVConfigChangeReg) {
+    TRACY_FUNC(sceAVConfigChangeReg);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigClearAutoSuspend2) {
+    TRACY_FUNC(sceAVConfigClearAutoSuspend2);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigDisplayOn) {
+    TRACY_FUNC(sceAVConfigDisplayOn);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigGetAcStatus) {
+    TRACY_FUNC(sceAVConfigGetAcStatus);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigGetBtVol) {
+    TRACY_FUNC(sceAVConfigGetBtVol);
     return UNIMPLEMENTED();
 }
 
-EXPORT(int, sceAVConfigGetConnectedAudioDevice) {
+EXPORT(int, sceAVConfigGetConnectedAudioDevice)
+    TRACY_FUNC(sceAVConfigGetConnectedAudioDevice);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigGetDisplayMaxBrightness) {
+    TRACY_FUNC(sceAVConfigGetDisplayMaxBrightness);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigGetMasterVol) {
+    TRACY_FUNC(sceAVConfigGetMasterVol);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigGetShutterVol) {
+    TRACY_FUNC(sceAVConfigGetShutterVol);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigGetSystemVol) {
+    TRACY_FUNC(sceAVConfigGetSystemVol);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigGetVolCtrlEnable) {
+    TRACY_FUNC(sceAVConfigGetVolCtrlEnable);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiCecCmdOneTouchPlay) {
+    TRACY_FUNC(sceAVConfigHdmiCecCmdOneTouchPlay);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiCecDisable) {
+    TRACY_FUNC(sceAVConfigHdmiCecDisable);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiCecEnable) {
+    TRACY_FUNC(sceAVConfigHdmiCecEnable);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiClearCecInfo) {
+    TRACY_FUNC(sceAVConfigHdmiClearCecInfo);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiGetCecInfo) {
+    TRACY_FUNC(sceAVConfigHdmiGetCecInfo);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiGetMonitorInfo) {
+    TRACY_FUNC(sceAVConfigHdmiGetMonitorInfo);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiGetOutScalingRatio) {
+    TRACY_FUNC(sceAVConfigHdmiGetOutScalingRatio);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiSetOutScalingRatio) {
+    TRACY_FUNC(sceAVConfigHdmiSetOutScalingRatio);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiSetResolution) {
+    TRACY_FUNC(sceAVConfigHdmiSetResolution);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigHdmiSetRgbRangeMode) {
+    TRACY_FUNC(sceAVConfigHdmiSetRgbRangeMode);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigMuteOn) {
+    TRACY_FUNC(sceAVConfigMuteOn);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigRegisterCallback) {
+    TRACY_FUNC(sceAVConfigRegisterCallback);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSendVolKey) {
+    TRACY_FUNC(sceAVConfigSendVolKey);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSetAutoDisplayDimming) {
+    TRACY_FUNC(sceAVConfigSetAutoDisplayDimming);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSetAutoSuspend) {
+    TRACY_FUNC(sceAVConfigSetAutoSuspend);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSetAutoSuspend2) {
+    TRACY_FUNC(sceAVConfigSetAutoSuspend2);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSetDisplayBrightness) {
+    TRACY_FUNC(sceAVConfigSetDisplayBrightness);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSetDisplayColorSpaceMode) {
+    TRACY_FUNC(sceAVConfigSetDisplayColorSpaceMode);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSetMasterVol) {
+    TRACY_FUNC(sceAVConfigSetMasterVol);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigSetSystemVol) {
+    TRACY_FUNC(sceAVConfigSetSystemVol);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigUnRegisterCallback) {
+    TRACY_FUNC(sceAVConfigUnRegisterCallback);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigWriteMasterVol) {
+    TRACY_FUNC(sceAVConfigWriteMasterVol);
     return UNIMPLEMENTED();
 }
 
 EXPORT(int, sceAVConfigWriteRegSystemVol) {
+    TRACY_FUNC(sceAVConfigWriteRegSystemVol);
     return UNIMPLEMENTED();
 }
 
